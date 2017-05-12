dc02d199e8919ebaafe51dfe17cd0752eba10a36
mdhtr <mdhtr@users.noreply.github.com>
2017-05-12 10:31:28 +0200

    Serving a static index.html file

--- http_server.js
+++ http_server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = 8080;
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    res.setHeader('content-type', 'text/html');
+    res.sendFile(path.join(__dirname, './index.html'));
+});
 
 app.listen(port, function () {
     console.log('Hello World server listening on port %d!', port)
