9840a0324db9fb71cfa7273f7c70c81189a9d71e
mdhtr <mdhtr@users.noreply.github.com>
2017-05-12 11:14:14 +0200

    Webpack copy static files & Express serve static folder

--- http_server.js
+++ http_server.js
@@ -3,14 +3,7 @@ var path = require('path');
 var app = express();
 var port = 8080;
 
-app.get('/', function (req, res) {
-    res.setHeader('content-type', 'text/html');
-    res.sendFile(path.join(__dirname, './src/index.html'));
-});
-app.get('/bundle.js', function (req, res) {
-    res.setHeader('content-type', 'application/javascript');
-    res.sendFile(path.join(__dirname, './dist/bundle.js'));
-});
+app.use('/', express.static(path.join(__dirname, 'dist')))
 
 app.listen(port, function () {
     console.log('Hello World server listening on port %d!', port)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: './dist/bundle.js'
-    }
+        filename: 'dist/bundle.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([{from: './src/static', to: './dist' }])
+    ]
 }
